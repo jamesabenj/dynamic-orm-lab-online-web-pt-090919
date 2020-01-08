a2f4aaa0f3ae269d1078012f836d354cdfff80c4
James Benjamin <james.abenj@gmail.com>
2020-01-08 17:10:40 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -54,7 +54,7 @@ class InteractiveRecord
 
   def self.find_by(attribute)
     sql = "SELECT * FROM #{self.table_name} WHERE #{attribute} = ?"
-    DB[:conn].execute(sql, attribute)
+    DB[:conn].execute(sql)
   end
 
 end
